153f672046eb792af8a44d902cd9b38bbab24142
guy <guytah@gmail.com>
2016-07-01 11:36:32 +0300

    change 2

--- Lab7 + 8/Lab8/Program.cs
+++ Lab7 + 8/Lab8/Program.cs
@@ -11,7 +11,7 @@ namespace Lab8
 
         public delegate bool CustomerFilter(CustomersApp.Customer person);
 
-        public ICollection<CustomersApp.Customer> GetCustomers(ICollection<CustomersApp.Customer> collection, CustomerFilter filter)
+        public static ICollection<CustomersApp.Customer> GetCustomers(ICollection<CustomersApp.Customer> collection, CustomerFilter filter)
         {
             LinkedList<CustomersApp.Customer> list = new LinkedList<CustomersApp.Customer>();
 
@@ -26,7 +26,7 @@ namespace Lab8
             return list;
         }
 
-        public bool FilterMethod(CustomersApp.Customer person)
+        public static bool FilterAK(CustomersApp.Customer person)
         {
                 string name = person.Name;
 
@@ -37,28 +37,77 @@ namespace Lab8
                 return false;
         }
 
+        public static bool FilterLZ(CustomersApp.Customer person)
+        {
+            string name = person.Name;
+
+            if (name[0] >= 'L' && name[0] <= 'Z')
+            {
+                return true;
+            }
+            return false;
+        }
+
+
         static void Main(string[] args)
         {
            
             LinkedList<CustomersApp.Customer> customers = new LinkedList<CustomersApp.Customer>();
-            customers.AddLast(new CustomersApp.Customer("Guy",125,"Givat Shmuel"));
+            customers.AddLast(new CustomersApp.Customer("Guy",86,"Givat Shmuel"));
             customers.AddLast(new CustomersApp.Customer("Dana",134,"Givat Shmuel"));
-            customers.AddLast(new CustomersApp.Customer("Aviko",132,"Givat Shmuel"));
+            customers.AddLast(new CustomersApp.Customer("Aviko",93,"Givat Shmuel"));
             customers.AddLast(new CustomersApp.Customer("Meshi",154,"Givat Shmuel"));
-            customers.AddLast(new CustomersApp.Customer("Or",162,"Givat Shmuel"));
+            customers.AddLast(new CustomersApp.Customer("Or",54,"Givat Shmuel"));
             customers.AddLast(new CustomersApp.Customer("Idan",171,"Givat Shmuel"));
 
-            //CustomerFilter filter = /* Create a class instead of program for this filter method */ Program.FilterMethod;
-            
-            /*
-            ICollection<CustomersApp.Customer> customers2 = new LinkedList<CustomersApp.Customer>();
+            Console.WriteLine("Before the filter:");
+            foreach (CustomersApp.Customer person in customers)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("using the A-K filter:");
+            Console.WriteLine();
+
+            ICollection<CustomersApp.Customer> customers2;
+            CustomerFilter filter = Program.FilterAK;
             customers2 = GetCustomers(customers, filter);
+        
 
             foreach (CustomersApp.Customer person in customers2)
             {
                 Console.WriteLine(person.ToString());
             }
-             * */
+
+            Console.WriteLine();
+            Console.WriteLine("using the L-Z filter:");
+            Console.WriteLine();
+
+            ICollection<CustomersApp.Customer> customers3;
+            CustomerFilter filter2 = Program.FilterLZ;
+            customers3 = GetCustomers(customers, filter2);
+
+            foreach (CustomersApp.Customer person in customers3)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+
+            CustomerFilter filter3 = (CustomersApp.Customer person) => person.ID < 100;
+
+            Console.WriteLine();
+            Console.WriteLine("using the ID < 100 filter:");
+            Console.WriteLine();
+
+            ICollection<CustomersApp.Customer> customers4;
+            customers4 = GetCustomers(customers, filter3);
+
+            foreach (CustomersApp.Customer person in customers4)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
         }
     }
 }
---
+++ Lab7 + 8/MailSystem/MailManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSystem
+{
+    class MailManager
+    {
+        public delegate void EventHandler<MailArrivedEventArgs>(object sender, MailArrivedEventArgs e) where MailArrivedEventArgs : EventArgs;
+    }
+}
